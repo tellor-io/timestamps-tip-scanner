9584486769b4eeb7abfafd3e3515f54450a69f44
akrem <akremstudy@gmail.com>
2022-09-26 22:33:25 -0400

    setup tools to pip install

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-def read_requirements():
-    with open('requirements.txt', 'r') as req:
-        content = req.read()
-        requirements = content.split('\n')
-
-    return requirements
-
-
-setup(
-    name='timestamps-scanner',
-    version='0.1.0',
-    packages=find_packages(),
-    include_package_date=True,
-    install_requires=read_requirements(),
-    entry_points="""
-        [console_scripts]
-        scanner=src
-    """,
-)
+setup()
