b83ee9729a2a1e8e954ac884d7098548e6174344
akrem <akremstudy@gmail.com>
2022-09-26 22:37:18 -0400

    moved dir for pip install

---
+++ src/timestamps_tip_scanner/timestamps_scanner.py
@@ -0,0 +1,103 @@
+import json
+import time
+import os
+
+from tqdm import tqdm
+from dotenv import load_dotenv
+from timestamps_tip_scanner.event_scanner import EventScanner
+from timestamps_tip_scanner.jsonified_state import JSONifiedState
+from timestamps_tip_scanner.utils import fallback_input
+from timestamps_tip_scanner.utils import w3_instance
+from pathlib import Path
+
+
+print(f"env loaded: {load_dotenv()}")
+Networks = {"mumbai": os.getenv("MUMBAI_NODE")}
+
+def run(network, reporter=None, starting_block=None):
+
+    api_url = Networks[network]
+
+    w3 = w3_instance(api_url)
+    # Prepare contract object
+    # Read contract ABIs from json files
+    _abi_folder = Path(__file__).resolve().parent / "abi"
+    print(_abi_folder)
+    with open(f"{_abi_folder}/tellorflex.json") as tellorflex_abi:
+        tellorflex_abi = json.load(tellorflex_abi)
+
+    tellorflex_address = fallback_input("TELLOR_FLEX_ADDRESS")
+    tellorflex_contract = w3.eth.contract(
+        address=tellorflex_address, abi=tellorflex_abi
+    )
+
+    # Restore/create our persistent state
+    state = JSONifiedState()
+    state.reset(starting_block)
+
+    try:
+        max_batch_scan_size = int(os.getenv("BATCH_SIZE", 3499))
+    except ValueError:
+        max_batch_scan_size = 3499
+        print(
+            f"Unable to read env variable, using default batch size: {max_batch_scan_size}"
+        )
+
+    # chain_id: int, web3: Web3, abi: dict, state: EventScannerState, events: List, filters: {}, max_chunk_scan_size: int=10000
+    scanner = EventScanner(
+        web3=w3,
+        state=state,
+        reporter=reporter,
+        contract=tellorflex_contract,
+        events=[tellorflex_contract.events.NewReport],
+        filters={"address": tellorflex_address},
+        # Infura max block ranger
+        max_chunk_scan_size=max_batch_scan_size,
+    )
+
+    # Assume we might have scanned the blocks all the way to the last Ethereum block
+    # that mined a few seconds before the previous scan run ended.
+    # Because there might have been a minor Etherueum chain reorganisations
+    # since the last scan ended, we need to discard
+    # the last few blocks from the previous scan results.
+    chain_reorg_safety_blocks = 10
+    scanner.delete_potentially_forked_block_data(
+        state.get_last_scanned_block() - chain_reorg_safety_blocks
+    )
+
+    # Scan from [last block scanned] - [latest ethereum block]
+    # Note that our chain reorg safety blocks cannot go negative
+
+    start_block = state.get_last_scanned_block()
+    end_block = scanner.get_suggested_scan_end_block()
+
+    blocks_to_scan = end_block - start_block
+
+    print(f"Scanning events from blocks {start_block} - {end_block}")
+
+    # Render a progress bar in the console
+    start = time.time()
+    with tqdm(total=blocks_to_scan) as progress_bar:
+
+        def _update_progress(start, end, current, chunk_size, events_count):
+            progress_bar.set_description(
+                f"Current block: {current}, blocks in a scan batch: {chunk_size}, events processed in a batch {events_count}"
+            )
+            progress_bar.update(chunk_size)
+
+        # Run the scan
+        result, total_chunks_scanned = scanner.scan(
+            start_block,
+            end_block,
+            progress_callback=_update_progress,
+            start_chunk_size=max_batch_scan_size,
+        )
+
+    state.save()
+    duration = time.time() - start
+    print(
+        f"Scanned total {len(result)} TellorFlex NewReport events, in {duration} seconds, total {total_chunks_scanned} chunk scans performed"
+    )
+
+    return state
+
---
+++ src/timestamps_tip_scanner/utils.py
@@ -0,0 +1,133 @@
+import os
+import math
+import json
+from web3 import HTTPProvider
+from web3 import Web3
+from dataclasses import dataclass
+from web3.contract import Contract
+from eth_utils import to_checksum_address
+
+
+@dataclass
+class FeedDetails:
+    """Data types for feed details contract response"""
+
+    reward: int
+    balance: int
+    startTime: int
+    interval: int
+    window: int
+    priceThreshold: int
+    feedsWithFundingIndex: int
+
+
+@dataclass
+class Tip:
+    """Data type for tips struct in autopay contract"""
+
+    amount: int
+    timestamp: int
+
+
+def fallback_input(_key: str):
+    val = os.getenv(_key, None)
+    if not val:
+        return input(f"{_key}:\n")
+    print(f"{_key} set!")
+    return val
+
+
+def one_time_tips(tips_lis: tuple, timestamp: int, timestamp_before: int):
+    """Check timestamps for one time tips"""
+    count = len(tips_lis)
+    if count > 0:
+        mini = 0
+        maxi = count
+        while maxi - mini > 1:
+            mid = int((maxi + mini) / 2)
+            tip_mid = Tip(*tips_lis[mid])
+            if tip_mid.timestamp > timestamp:
+                maxi = mid
+            else:
+                mini = mid
+        tips = Tip(*tips_lis[mini])
+        conditions = (
+            timestamp_before < tips.timestamp,
+            timestamp > tips.timestamp,
+            tips.amount > 0,
+        )
+        return all(conditions)
+
+
+def is_timestamp_first_in_window(
+    timestamp_before: int,
+    timestamp_to_check: int,
+    feed_start_timestamp: int,
+    feed_window: int,
+    feed_interval: int,
+) -> bool:
+    """
+    Calculates to check if timestamp(timestamp_to_check) is first in window
+
+    Return: bool
+    """
+    # Number of intervals since start time
+    num_intervals = math.floor(
+        (timestamp_to_check - feed_start_timestamp) / feed_interval
+    )
+    # Start time of latest submission window
+    current_window_start = feed_start_timestamp + (feed_interval * num_intervals)
+    eligible = [
+        (timestamp_to_check - current_window_start) < feed_window,
+        timestamp_before < current_window_start,
+    ]
+    return all(eligible)
+
+
+def evm_transaction(
+    contract_factory: Contract,
+    func_name: str,
+    w3: Web3,
+    wallet_address: str,
+    private_key: str,
+    **kwargs,
+):
+    gas = int(os.getenv("GAS", 400000))
+    gas_multiplier = int(os.getenv("GAS_MULTIPLIER", 2))
+
+    wallet_nonce = w3.eth.get_transaction_count(wallet_address)
+    txn_build = contract_factory.get_function_by_name(func_name)(
+        **kwargs
+    ).buildTransaction(
+        dict(
+            nonce=int(wallet_nonce),
+            gasPrice=int(w3.eth.gas_price * gas_multiplier),
+            gas=gas,
+        )
+    )
+    signed_txn = w3.eth.account.signTransaction(txn_build, private_key)
+    # Send the transaction
+    transaction_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    transaction_hash = transaction_hash.hex()
+    print(f"{func_name} txn: {transaction_hash}")
+    receipt = w3.eth.wait_for_transaction_receipt(transaction_hash, timeout=480)
+    return receipt
+
+
+def autopay_factory(address: str, w3: Web3):
+    with open("abi/autopay.json") as f:
+        autopay_abi = json.load(f)
+
+    return w3.eth.contract(address=address, abi=autopay_abi)
+
+
+def w3_instance(node_url):
+
+    provider = HTTPProvider(node_url)
+
+    # Remove the default JSON-RPC retry middleware
+    # as it correctly cannot handle eth_getLogs block range
+    # throttle down.
+    provider.middlewares.clear()
+
+    return Web3(provider)
